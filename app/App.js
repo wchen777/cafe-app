8c8d7f7524fb1b322d1253a0630c347a316971e7
Nicole Kim <na_yeon_kim@brown.edu>
2021-03-30 11:18:09 -0700

    added

--- app/App.js
+++ app/App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from './components/Landing';
+import SignUpScreen from './components/SignUp';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='LandingScreen'>
+        <Stack.Screen
+          name='Landing'
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='SignUp'
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ app/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Image, Text, TextField, TextArea, Button, Colors} from 'react-native-ui-lib';
+
+export default function SignUp({navigation}) { 
+    return (
+        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFFDFC'}}>
+        </View>
+    )
+}
