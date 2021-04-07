36c56d3af53cccae5edbe76d41267da2dcdad601
Nicole Kim <na_yeon_kim@brown.edu>
2021-04-07 12:05:56 -0700

    fixed textpost bug

--- app/components/cards/TextCard.js
+++ app/components/cards/TextCard.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { View, Text, Colors, Card } from 'react-native-ui-lib';
 
-
-export default function TextCard( { navigation , textPosts} ) {
+//use map here or no?
+export default function TextCard( { navigation , textPost} ) {
     return (
-        <View>
-            {textPosts.map((post) => 
                 <Card
                     key={1}
                     style={{ marginBottom: 20 }}
@@ -16,19 +14,19 @@ export default function TextCard( { navigation , textPosts} ) {
                     
                     <View padding-20 bg-white borderRadius={20}>
                         <Text text40 color={Colors.grey10} marginV-14>
-                            {post.title}
+                            {textPost.title}
                     </Text>
         
                         <View row>
                             <Text text60 color={Colors.orange30} >
-                                {post.username}
+                                {textPost.username}
                         </Text>
                             <Text text60 color={Colors.grey10}> | </Text>
-                            <Text text70 color={Colors.red50}>{post.category} </Text>
+                            <Text text70 color={Colors.red50}>{textPost.category} </Text>
                         </View>
         
                         <Text text70 color={Colors.grey10} marginT-10>
-                            {post.description}
+                            {textPost.description}
                     </Text>
         
                         <View>
@@ -50,9 +48,5 @@ export default function TextCard( { navigation , textPosts} ) {
                         </View>
                     </View>
                 </Card>
-            
-                )
-            }
-        </View>
     )
 }
