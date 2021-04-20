47b2d27b6598128ef26c57e043a40adf8f3d2ff4
Nicole Kim <na_yeon_kim@brown.edu>
2021-04-19 20:15:17 -0700

    code clean

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -6,17 +6,6 @@ import "firebase/firestore";
 
 export const AuthProvider = ({ children }) => {
 
-    // async function getUserInfo() {
-    //     let currentUserUID = firebase.auth().currentUser.uid;
-    //     let doc = await firebase
-    //         .firestore()
-    //         .collection('users')
-    //         .doc(currentUserUID)
-    //         .get();
-
-    //     return doc.data()
-    // }
-
     const [user, setUser] = useState(null);
     const [userData, setUserData] = useState({})
 
@@ -36,5 +25,3 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const AuthContext = createContext({});
-
-//   export const useAuthContext = useContext(AuthContext)
